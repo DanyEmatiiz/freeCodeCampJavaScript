8a91bc94c4ad68cd3dcd34e6db7ab2b334ad80f6
Daniel <encisodaniel2002@gmail.com>
2025-01-28 00:46:35 -0500

    respuesta de la tarea

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 /*
 // Ejemplos de variables mal definidas en JavaScript
 
-var 123abc = "hello";
-var my variable = 10;
-let @name = "John";
-const -pi = 3.14;
-var var = 25;
-let 2days = "Monday";
-const user-name = "Alice";
-var !city = "Tokyo";
-let %discount = 5;
-const color# = "red";
-var home address = "123 Main St";
-let new-user = true;
-const float$number = 3.14;
-var *price = 100;
-let 5students = ["John", "Jane"];
-const pi value = 3.1416;
-var user name = "Alice";
-let !isActive = true;
-const value% = 42;
-var $totalAmount = 500;
+var 123abc = "hello";  var abc123 = "hello"
+var my variable = 10;  var MyVariable = 10
+let @name = "John";   let name = "john"
+const -pi = 3.14;   const _pi = 3.14
+var var = 25; var rav = 25
+let 2days = "Monday"; let days2 = "monday"
+const user-name = "Alice"; const user_name = "alice"
+var !city = "Tokyo";  var city = "tokyo"
+let %discount = 5; let discount = 5
+const color# = "red"; const color = "red"
+var home address = "123 Main St"; var HomeAdress "123 main st"
+let new-user = true;  let new_user = "true"
+const float$number = 3.14;  const float$nomber = 3.14
+var *price = 100; var price = 100
+let 5students = ["John", "Jane"]; let students = ["jhon","jane"]
+const pi value = 3.1416; const PiValue = 3.1416
+var user name = "Alice"; var UserName = "alice"
+let !isActive = true; let IsActive = "true"
+const value% = 42; const value = 42 
+var $totalAmount = 500;var $totalAlmount = 500
 
 */
