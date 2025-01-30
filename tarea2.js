0bd245435262d1110407465025114ba1271c7782
Daniel <encisodaniel2002@gmail.com>
2025-01-30 14:37:22 -0500

    borrador 1

--- tarea2.js
+++ tarea2.js
@@ -17,3 +17,24 @@ actuliza la variable que contenga los puntos de vida multiplicados x 14.
 
 actualiza la variable que contenga los puntos de vida después de ser envenenado y perder dos veces la mitad de sus puntos de vida.
 */
+/*valores*/
+let warName = cotter
+let liveCotter = 100
+let robotDamage = -10
+let robotPoison = liveCotter/=2
+/*acciones*/
+/* robot golpea a cotter */
+liveCotter += robotDamage /*90*/
+const text =  "cotter" + "fue atacado y perdio" + "-10 puntos de vida" + "ahora tiene 90 puntos de vida"
+liveCotter *=14 /*1260*/
+robotPoison += liveCotter
+robotPoison += liveCotter
+
+
+
+
+
+
+
+
+
